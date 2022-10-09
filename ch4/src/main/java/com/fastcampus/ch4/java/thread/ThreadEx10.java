0f1cb5d2b5f9e50cf1292aed78a9012a1a0e78d6
ByongE <kbh25963@gmail.com>
2022-10-09 22:51:05 +0900

    Study thread

--- ch4/src/main/java/com/fastcampus/ch4/java/thread/ThreadEx10.java
+++ ch4/src/main/java/com/fastcampus/ch4/java/thread/ThreadEx10.java
@@ -1,54 +1,37 @@
 package com.fastcampus.ch4.java.thread;
 
-class ThreadEx17 {
+class ThreadEx22 {
 	public static void main(String args[]) {
-		ThreadEx17_1 th1 = new ThreadEx17_1("*");
-		ThreadEx17_1 th2 = new ThreadEx17_1("**");
-		ThreadEx17_1 th3 = new ThreadEx17_1("***");
-		th1.start();
-		th2.start();
-		th3.start();
-
-		try {
-			Thread.sleep(2000);
-			th1.suspend();		
-			Thread.sleep(2000);
-			th2.suspend();
-			Thread.sleep(3000);
-			th1.resume();
-			Thread.sleep(3000);
-			th1.stop();
-			th2.stop();
-			Thread.sleep(2000);
-			th3.stop();
-		} catch (InterruptedException e) {}
+		Runnable r = new RunnableEx22();
+		new Thread(r).start();
+		new Thread(r).start();
 	}
 }
 
-class ThreadEx17_1 implements Runnable {
-	boolean suspended = false;
-	boolean stopped   = false;
+class Account {
+	private int balance = 1000; // private으로 해야 동기화가 의미가 있다.
 
-	Thread th;
-
-	ThreadEx17_1(String name) {
-		th = new Thread(this, name); // Thread(Runnable r, String name)
+	public  int getBalance() {
+		return balance;
 	}
+
+	public synchronized void withdraw(int money){ // synchronized로 메서드를 동기화
+		if(balance >= money) {
+			try { Thread.sleep(1000);} catch(InterruptedException e) {}
+			balance -= money;
+		}
+	} // withdraw
+}
+
+class RunnableEx22 implements Runnable {
+	Account acc = new Account();
 
 	public void run() {
-		while(!stopped) {
-			if(!suspended) {
-               System.out.println(Thread.currentThread().getName());
-				try {
-					Thread.sleep(1000);
-				} catch(InterruptedException e) {}			
-			}
+		while(acc.getBalance() > 0) {
+			// 100, 200, 300중의 한 값을 임으로 선택해서 출금(withdraw)
+			int money = (int)(Math.random() * 3 + 1) * 100;
+			acc.withdraw(money);
+			System.out.println("balance:"+acc.getBalance());
 		}
-		System.out.println(Thread.currentThread().getName() + " - stopped");
-	}
-
-	public void suspend() { suspended = true;  }
-	public void resume()  { suspended = false; }
-	public void stop()    { stopped   = true;  }
-	public void start()   { th.start();        }
+	} // run()
 }
