c35a8d32659a48ac2888ee196699f602a323e0ac
ByongE <kbh25963@gmail.com>
2022-10-15 20:47:42 +0900

    Study exception

--- ch4/src/main/java/com/fastcampus/ch4/java/exception/ExceptionEx12.java
+++ ch4/src/main/java/com/fastcampus/ch4/java/exception/ExceptionEx12.java
@@ -1,16 +1,21 @@
 package com.fastcampus.ch4.java.exception;
 
-class ExceptionEx13 {
-	public static void main(String[] args) {
-		method1(); 	  // 같은 클래스내의 static멤버이므로 객체생성없이 직접 호출가능.
-  	}	// main메서드의 끝
+class ExceptionEx17 {
+	public static void main(String[] args) 
+	{
+		try  {
+			method1();		
+		} catch (Exception e)	{
+			System.out.println("main메서드에서 예외가 처리되었습니다.");
+		}
+	}	// main메서드의 끝
 
-	static void method1() {
+	static void method1() throws Exception {
 		try {
-		     throw new Exception();
+			throw new Exception();
 		} catch (Exception e) {
 			System.out.println("method1메서드에서 예외가 처리되었습니다.");
-			e.printStackTrace();
+			throw e;			// 다시 예외를 발생시킨다.
 		}
-	}	// method1의 끝
+	}	// method1메서드의 끝
 }
