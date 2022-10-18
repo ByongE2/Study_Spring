051ab2553e2262909ef75c8a26a9b06a2a0c8124
ByongE <kbh25963@gmail.com>
2022-10-18 22:08:20 +0900

    Study array

--- ch4/src/main/java/com/fastcampus/ch4/java/thread/ThreadEx1.java
+++ ch4/src/main/java/com/fastcampus/ch4/java/thread/ThreadEx1.java
@@ -1,31 +1,22 @@
 package com.fastcampus.ch4.java.thread;
 
-class ArrayEx10 {
+class ArrayEx11 {
 	public static void main(String[] args) {
-		int[] numArr = new int[10];
+		int[] numArr  = new int[10];
+		int[] counter = new int[10];
 
 		for (int i=0; i < numArr.length ; i++ ) {
-			System.out.print(numArr[i] = (int)(Math.random() * 10));
+			numArr[i] = (int)(Math.random() * 10); // 0~9의 임의의 수를 배열에 저장
+			System.out.print(numArr[i]);
 		}
 		System.out.println();
 
-		for (int i=0; i < numArr.length-1 ; i++ ) {
-			boolean changed = false;	// 자리바꿈이 발생했는지를 체크한다.
+		for (int i=0; i < numArr.length ; i++ ) {
+			counter[numArr[i]]++;
+		}
 
-			for (int j=0; j < numArr.length-1-i ;j++) {
-				if(numArr[j] > numArr[j+1]) { // 옆의 값이 작으면 서로 바꾼다.
-					int temp = numArr[j];
-					numArr[j] = numArr[j+1];
-					numArr[j+1] = temp;
-					changed = true;	// 자리바꿈이 발생했으니 changed를 true로.
-				}
-			} // end for j
-
-			if (!changed) break;	// 자리바꿈이 없으면 반복문을 벗어난다.
-
-			for(int k=0; k<numArr.length;k++)
-				System.out.print(numArr[k]); // 정렬된 결과를 출력한다.
-			System.out.println();
-		} // end for i
+		for (int i=0; i < numArr.length ; i++ ) {
+			System.out.println( i +"의 개수 :"+ counter[i]);
+		}
 	} // main의 끝
 }
