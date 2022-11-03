e113761b6846682c9c4abfa1e3ababb0e3a0e5cc
ByongE <kbh25963@gmail.com>
2022-11-03 17:40:17 +0900

    Study ch15&practice

---
+++ ch4/src/main/java/com/fastcampus/ch4/java/practice/Ex15_13.java
@@ -0,0 +1,8 @@
+package com.fastcampus.ch4.java.practice;
+
+class Ex15_13 {
+	public static void main(String[] args) {
+		System.out.println("out : Hello World!");
+		System.err.println("err : Hello World!");
+	}
+}
