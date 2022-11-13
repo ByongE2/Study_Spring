434195fe075a450eb5e25015064d977f1841e6dc
ByongE <kbh25963@gmail.com>
2022-11-13 16:39:21 +0900

    Study ch11&practice

---
+++ ch4/src/main/java/com/fastcampus/ch4/java/practice/Ex11_15.java
@@ -0,0 +1,16 @@
+package com.fastcampus.ch4.java.practice;
+
+import java.util.*;
+
+class Ex11_15 {
+	public static void main(String[] args) {
+		TreeSet set = new TreeSet();
+		int[] score = {80, 95, 50, 35, 45, 65, 10, 100};
+
+		for(int i=0; i < score.length; i++)
+			set.add(new Integer(score[i]));
+
+		System.out.println("50º¸´Ù ÀÛÀº °ª :" + set.headSet(new Integer(50)));
+		System.out.println("50º¸´Ù Å« °ª :"  + set.tailSet(new Integer(50)));
+	}
+}
