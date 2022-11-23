141fc3dd3c052b84bdc0cc44b04604ecb74fa2da
ByongE <kbh25963@gmail.com>
2022-11-23 23:14:55 +0900

    Study ch7&practice

---
+++ ch4/src/main/java/com/fastcampus/ch4/java/practice/Ex7_2.java
@@ -0,0 +1,20 @@
+package com.fastcampus.ch4.java.practice;
+
+class Ex7_2 {
+	public static void main(String args[]) {
+		Child c = new Child();
+		c.method();
+	}
+}
+
+class Parent { int x=10; }
+
+class Child extends Parent {
+	int x=20;
+
+	void method() {
+		System.out.println("x=" + x);
+		System.out.println("this.x=" + this.x);
+		System.out.println("super.x="+ super.x);
+	}
+}
