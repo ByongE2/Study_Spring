b6ba22c0672e81fbee9d97cab08eee4b9bcfde8b
ByongE <kbh25963@gmail.com>
2022-11-30 22:38:22 +0900

    Study ch8&practice

---
+++ ch4/src/main/java/com/fastcampus/ch4/java/practice/Ex8_7.java
@@ -0,0 +1,7 @@
+package com.fastcampus.ch4.java.practice;
+
+class Ex8_7 {
+	public static void main(String[] args) {
+		throw new Exception();		// ExceptionÀ» °íÀÇ·Î ¹ß»ý½ÃÅ²´Ù.
+	}
+}
---
+++ ch4/src/main/java/com/fastcampus/ch4/java/practice/Ex8_8.java
@@ -0,0 +1,7 @@
+package com.fastcampus.ch4.java.practice;
+
+class Ex8_8 {
+	public static void main(String[] args) {
+		throw new RuntimeException();	// RuntimeExceptionÀ» °íÀÇ·Î ¹ß»ý½ÃÅ²´Ù.
+	}
+}
