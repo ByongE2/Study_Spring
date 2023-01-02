81fe2103993c82e75c0aeb4760e119037fef40d8
ByongE <kbh25963@gmail.com>
2023-01-02 23:30:26 +0900

    Study ch4&practice

---
+++ ch4/src/main/java/com/fastcampus/ch4/java/practice/Ex4_1.java
@@ -0,0 +1,11 @@
+package com.fastcampus.ch4.java.practice;
+
+class Ex4_1 {
+	public static void main(String args[]) {
+		int score = 80;
+
+		if (score > 60) {
+			System.out.println("ÇÕ°ÝÀÔ´Ï´Ù.");
+		}
+	}
+}
