7adb556eb478591e57184f6d411bf77b13e69cfc
ByongE <kbh25963@gmail.com>
2023-01-04 23:36:45 +0900

    Study ch4&practice

---
+++ ch4/src/main/java/com/fastcampus/ch4/java/practice/Ex4_8.java
@@ -0,0 +1,9 @@
+package com.fastcampus.ch4.java.practice;
+
+class Ex4_8 {
+	public static void main(String args[]) {
+		for (int i = 1; i <= 3; i++) { // °ýÈ£{}¾ÈÀÇ ¹®ÀåÀ» 3¹ø ¹Ýº¹
+			System.out.println("Hello");
+		}
+	}
+}
