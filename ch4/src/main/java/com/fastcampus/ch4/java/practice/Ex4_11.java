5633f99a9535186a8bf949779030824eddeda7fd
ByongE <kbh25963@gmail.com>
2023-01-05 22:16:04 +0900

    Study ch4&practice

---
+++ ch4/src/main/java/com/fastcampus/ch4/java/practice/Ex4_11.java
@@ -0,0 +1,15 @@
+package com.fastcampus.ch4.java.practice;
+
+import java.util.*;
+
+class Ex4_11 {
+	public static void main(String[] args) { 
+
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	} // mainÀÇ ³¡
+}
