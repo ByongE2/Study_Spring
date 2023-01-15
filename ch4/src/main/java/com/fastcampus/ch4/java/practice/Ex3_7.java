0712c6b9ee5c3bfe07a9f5b971a8bf7cb9c6b31d
ByongE <kbh25963@gmail.com>
2023-01-15 20:50:41 +0900

    Study ch3&practice

---
+++ ch4/src/main/java/com/fastcampus/ch4/java/practice/Ex3_7.java
@@ -0,0 +1,8 @@
+package com.fastcampus.ch4.java.practice;
+
+class Ex3_7 {
+	public static void main(String[] args) {
+		System.out.println(5/2);
+		System.out.println(5/(float)2);  //  (float)5/2ÀÇ °á°úµµ µ¿ÀÏ
+	}
+}
