cc128e29db393a07bc05b67993be1839f8a154b8
ByongE <kbh25963@gmail.com>
2023-01-17 21:17:39 +0900

    Study ch3&pracie

---
+++ ch4/src/main/java/com/fastcampus/ch4/java/practice/Ex3_9.java
@@ -0,0 +1,12 @@
+package com.fastcampus.ch4.java.practice;
+
+class Ex3_9 {
+	public static void main(String args[]) { 
+//		int a = 1_000_000;    // 1,000,000   1¹é¸¸ 
+//		int b = 2_000_000;    // 2,000,000   2¹é¸¸ 
+
+//		long c = a * b;       // a * b = 2,000,000,000,000 ?
+
+//		System.out.println(c);
+	}
+}
