14e9f5df2f2310fb50c8b26087626a720196e28d
ByongE <kbh25963@gmail.com>
2023-03-02 21:15:46 +0900

    Study ch02&practice

---
+++ ch4/src/main/java/com/fastcampus/ch4/java/practice/Ex2_6.java
@@ -0,0 +1,15 @@
+package com.fastcampus.ch4.java.practice;
+
+class Ex2_6 {
+	   public static void main(String args[]) {
+			int 	 x = 100;
+			double pi = 3.14;
+			char 	 ch = 'a';
+			String str = "abc";
+
+			System.out.println(x); 
+			System.out.println(pi); 
+			System.out.println(ch);
+			System.out.println(str);
+		}
+	}
