919689dacc5cc6da510434eeec5833b496d20882
ByongE <kbh25963@gmail.com>
2023-03-26 23:39:32 +0900

    Study Operator

---
+++ ch4/src/main/java/com/fastcampus/ch4/java/practice/OperatorEx6.java
@@ -0,0 +1,11 @@
+package com.fastcampus.ch4.java.practice;
+
+class OperatorEx6 { 
+    public static void main(String[] args) { 
+          byte a = 10; 
+          byte b = 20; 
+          byte c = a + b; 
+
+          System.out.println(c); 
+    } 
+} 
