ca40e2e86222a0962083cda044e2443e46b9ff0d
ByongE <kbh25963@gmail.com>
2023-03-27 09:07:50 +0900

    Study Operator

---
+++ ch4/src/main/java/com/fastcampus/ch4/java/practice/OperatorEx8.java
@@ -0,0 +1,12 @@
+package com.fastcampus.ch4.java.practice;
+
+class OperatorEx8 {
+	public static void main(String args[]) { 
+		int a = 1000000;    // 1,000,000   1¹é¸¸ 
+		int b = 2000000;    // 2,000,000   2¹é¸¸ 
+
+		long c = a * b;     // a * b = 2,000,000,000,000 ?
+
+		System.out.println(c);
+	}
+}
